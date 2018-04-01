51ae1413ceba52b224d712191ccb24f15f7a3486
git4vikas <37271128+git4vikas@users.noreply.github.com>
2018-04-01 20:31:29 +0530

    Added organic and Inorganic classifier

--- Analyser.py
+++ Analyser.py
@@ -1,14 +1,14 @@
-import pandas as pd
-import json
+import pandas as pd, json, matplotlib.pyplot as plt
+
 with open('RawData.json') as data_file:
     data = json.load(data_file)
+df = pd.DataFrame(data['data'], columns=['N', 'P', 'K'])
 
-print(data)
-
-df = pd.DataFrame(data['data'],columns=['N','P','K'])
-print (df)
-import matplotlib.pyplot as plt
-
-df.cumsum()
+print(data['data'][0]['N'],len(data['data']))
+for i in range(len(data['data'])):
+    if int(data['data'][i]['N']) < 456 or int(data['data'][i]['P']) < 20 or int(data['data'][i]['K']) < 470:
+        print(data['data'][i]['date'],data['data'][i]['N'],data['data'][i]['P'],data['data'][i]['K'], "ORGANIC")
+    else:
+        print(data['data'][i]['date'],data['data'][i]['N'],data['data'][i]['P'],data['data'][i]['K'], "INORGANIC")
 df.plot()
 plt.show()
