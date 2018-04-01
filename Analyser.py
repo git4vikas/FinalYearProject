9c63e15f048570744911923478d6551562aa052d
git4vikas <37271128+git4vikas@users.noreply.github.com>
2018-04-01 21:17:10 +0530

    Reformatted

--- Analyser.py
+++ Analyser.py
@@ -4,11 +4,11 @@ with open('RawData.json') as data_file:
     data = json.load(data_file)
 df = pd.DataFrame(data['data'], columns=['N', 'P', 'K'])
 
-print(data['data'][0]['N'],len(data['data']))
+print(data['data'][0]['N'], len(data['data']))
 for i in range(len(data['data'])):
     if int(data['data'][i]['N']) < 456 or int(data['data'][i]['P']) < 20 or int(data['data'][i]['K']) < 470:
-        print(data['data'][i]['date'],data['data'][i]['N'],data['data'][i]['P'],data['data'][i]['K'], "ORGANIC")
+        print(data['data'][i]['date'], data['data'][i]['N'], data['data'][i]['P'], data['data'][i]['K'], "ORGANIC")
     else:
-        print(data['data'][i]['date'],data['data'][i]['N'],data['data'][i]['P'],data['data'][i]['K'], "INORGANIC")
+        print(data['data'][i]['date'], data['data'][i]['N'], data['data'][i]['P'], data['data'][i]['K'], "INORGANIC")
 df.plot()
 plt.show()
