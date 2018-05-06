091c5d5e0e1c27de11269e6e649b4553e899dc66
git4vikas <37271128+git4vikas@users.noreply.github.com>
2018-05-06 18:26:36 +0530

    Update Analyser.py

--- Analyser.py
+++ Analyser.py
@@ -1,14 +1,14 @@
 import pandas as pd, json, matplotlib.pyplot as plt
 
-with open('RawData.json') as data_file:
+with open('data1.json') as data_file:
     data = json.load(data_file)
-df = pd.DataFrame(data['data'], columns=['N', 'P', 'K'])
+df = pd.DataFrame(data['data'], columns=['Nitrogen', 'Phosphorus', 'Potassium'])
 
-print(data['data'][0]['N'], len(data['data']))
+print(data['data'][0]['Nitrogen'], len(data['data']))
 for i in range(len(data['data'])):
-    if int(data['data'][i]['N']) < 456 or int(data['data'][i]['P']) < 20 or int(data['data'][i]['K']) < 470:
-        print(data['data'][i]['date'], data['data'][i]['N'], data['data'][i]['P'], data['data'][i]['K'], "ORGANIC")
+    if int(data['data'][i]['Nitrogen']) < 456 and int(data['data'][i]['Phosphorus']) <25 and  int(data['data'][i]['Potassium']) < 470:
+        print(data['data'][i]['date'], data['data'][i]['Nitrogen'], data['data'][i]['Phosphorus'], data['data'][i]['Potassium'], "ORGANIC")
     else:
-        print(data['data'][i]['date'], data['data'][i]['N'], data['data'][i]['P'], data['data'][i]['K'], "INORGANIC")
+        print(data['data'][i]['date'], data['data'][i]['Nitrogen'], data['data'][i]['Phosphorus'], data['data'][i]['Potassium'], "INORGANIC")
 df.plot()
 plt.show()
